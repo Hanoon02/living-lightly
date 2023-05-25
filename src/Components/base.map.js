6b99bb70c71a39539bfdbe10571a71c5109138cd
Nirbhay Singh Narang <84955878+nirbhaysinghnarang@users.noreply.github.com>
2023-05-25 18:42:31 +0530

    Set up map with custom style.

---
+++ src/Components/base.map.js
@@ -0,0 +1,30 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import { env_vars } from '../Config/env';
+import { Box } from '@mui/material';
+
+
+mapboxgl.accessToken = env_vars.ACCESS_TOKEN
+
+export default function App() {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: env_vars.MAP_STYLE,
+            center: [-70.9, 42.35],
+            zoom: 13
+        });
+    });
+
+    useEffect(() => {
+        if (!map.current) return;
+    });
+
+    return (
+
+        <Box sx={{ width: '100vw', height: '100vh', padding:0 }} ref={mapContainer}  />
+    );
+}
---
+++ src/Config/env.js
@@ -0,0 +1,4 @@
+export const env_vars = {
+    "MAP_STYLE": process.env.REACT_APP_MAPBOX_STYLE_URL,
+    "ACCESS_TOKEN": process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
+}
