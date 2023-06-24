d1715d52dda558cd59ffb3daeaf3030096fcb827
Hanoon <90821506+Hanoon02@users.noreply.github.com>
2023-06-24 20:23:38 +0400

    Code Fix

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -13,7 +13,17 @@ export const ZOOM_OUT_LIMIT = 3
 //ASSET FILES
 export const MAP_OVERLAY_ASSET = "url(/Assets/Images/map_overlay.png)"
 export const INSET_MAP_OVERLAY_ASSET = "url(/Assets/Images/inset_map_overlay.png)"
-export const ROUTE_START_IMG = "/routeStart.png"
+
+export const ROUTE_START_IMG = "/Assets/Images/Map/routeStart.png"
+export const ROUTE_POINTER_IMG = "/Assets/Images/Map/routePointer.png"
+export const ARROW_TEXTURE = "/Assets/Images/Map/arrowTexture.png"
+export const ARROW_NEXT_IMG = "/Assets/Images/Map/arrowNext.png"
+export const ARROW_PREV_IMG = "/Assets/Images/Map/arrowPrev.png"
+
+export const ABOUT_SHOP_IMG = "/Assets/Images/About/shop.png"
+export const ABOUT_PATCH_IMG = "/Assets/Images/About/patch.png"
+export const ABOUT_ROUTE_IMG = "/Assets/Images/About/route.png"
+export const ABOUT_FOREST_IMG = "/Assets/Images/About/forest.png"
 
 export const COMPASS_ASSET = "url(/Assets/Images/compass_north.png)"
 export const ROUTE_MARKER_IMG = "url(/Assets/Images/routePointer.png)"
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,28 +1,28 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {ABOUT_SHOP_IMG, ABOUT_ROUTE_IMG, ABOUT_FOREST_IMG, ABOUT_PATCH_IMG} from "../../Constants/constants";
 
 export default function About() {
     return (
         <>
-            <div className={'absolute gotu-font'}>
-                <div className={'z-50 py-[100px] px-[50px]'}>
-                    <div className={'flex align-top justify-evenly'}>
+            <div className={"bg-[#D0BA89] bg-[url('../public/Assets/Images/map_overlay.png')] bg-contain bg-no-repeat bg-center gotu-font w-full"}>
+                <div className={'px-[50px] py-[50px]'}>
+                    <div className={"bg-center bg-cover bg-[url('../public/Assets/Images/inset_map_overlay.png')] flex align-top justify-evenly"}>
                         <div className={'grid content-start order border-3 border-black flex  flex-col w-1/2 align-center justify-center'}>
                             <div className={'flex h-[800px]'}>
                                 <div className={'grid content-end w-1/3border border-3 border-black'}>
-                                    <img src={require("../../Assets/About/shop.png")} alt={'shop'} className={''} />
+                                    <img src={ABOUT_SHOP_IMG} alt={'shop'} className={''} />
                                 </div>
                                 <div className={'grid brder border-3 order-black content-start w-2/3 z-50 brder border-3 border-black flex '}>
                                     <p className={'text-[39px] text-center text-[#894E35]'}>About Us</p>
-                                    <div>
-                                        <div className={''}><img src={require("../../Assets/About/brown.png")} alt={'patch'} className={'absolute -z-20'} /></div>
-                                        <p className={'text-[20px] pl-10 pt-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                    <div className={"bg-center bg-cover bg-[url('../public/Assets/Images/About/brown.png')]"}>
+                                        <p className={'text-[20px] pl-10 py-10'}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div cla    ssName={'w-1/2 flex flex-col align-center justify-center cursor-pointer'}>
-                            <button className={'rounded-full px-[100px] mx-[230px] py-2 text-[24px] bg-[#D0BA89]'}>
+                        <div className={'w-1/2 flex flex-col align-center justify-center cursor-pointer'}>
+                            <button className={'rounded-full px-[100px] mx-auto py-2 text-[24px] bg-[#D0BA89]'}>
                                 <Link to={'/base'}>Explore</Link>
                             </button>
                             <div className={'brder border-3 border-black h-[500px]'}>
@@ -38,20 +38,19 @@ export default function About() {
                                     </p>
                                 </div>
                                 <div className={'absolute boder border-3 border-black '}>
-                                    <img src={require("../../Assets/About/patch.png")} alt={'patch'} />
+                                    <img src={ABOUT_PATCH_IMG} alt={'patch'} />
                                 </div>
-                                <img src={require("../../Assets/About/route.png")} alt={'tree'} className={''} />
+                                <img src={ABOUT_ROUTE_IMG} alt={'route'} className={''} />
                             </div>
                             <div>
                                 <div className={'flex pt-5 pl-5'}>
-                                    <img src={require("../../Assets/About/forest.png")} alt={'tree'} className={''} />
+                                    <img src={ABOUT_FOREST_IMG} alt={'forest'} className={''} />
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <img src={require("../../Assets/About/map_overlay.png")} alt={'map'} className={'w-screen'} />
         </>
     );
 }
